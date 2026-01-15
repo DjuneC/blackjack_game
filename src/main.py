5894c237da22a2af9570abe6dfbd364cc6395815
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-14 22:43:09 -0800

    Reformat main file and create another module to handle deck operation

--- src/main.py
+++ src/main.py
@@ -1,38 +1,16 @@
 import random
 
 from full_deck import card_deck as total_deck
-
-def get_index_card(deck, card):
-    index_card = deck.index(card)
-    return index_card
-
-def remove_card_from_deck(deck, hand):
-    for card in hand:
-        index_in_deck = deck.index(card)
-        deck.remove(deck[index_in_deck])
-    
-def get_card(deck, quantity):
-    random_card = random.choices(deck, k = quantity)
-    
-    return random_card 
+from deck_operation import get_card, get_index_card, remove_card_from_deck
 
 def main():
-    print(len(total_deck))
     random.shuffle(total_deck)
 
     player_hand = get_card(total_deck, 2)
     computer_hand = get_card(total_deck, 2)
-    
-    print(player_hand)
-    print(computer_hand)
 
     remove_card_from_deck(total_deck, player_hand)
     remove_card_from_deck(total_deck, computer_hand)
-
-    print("***********************************")
-    
-    print(len(total_deck))
-
 
 if __name__ == "__main__":
     main()
